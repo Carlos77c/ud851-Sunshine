a529009b08821638ce796aa5806d9b5a56f11901
Carlos77c <Carlos77c@users.noreply.github.com>
2018-02-03 13:10:12 +0100

    S01.02-Exercise COMPLETED

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,43 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED (1) Create a field to store the weather display TextView
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        String[] testWeatherData = {
+                "Today, Jun 7 - Rainy - 1°C / 7°C",
+                "Tomorrow - Cloudy - 4°C / 9°C",
+                "Thursday - Rainy- 8°C / 11°C",
+                "Friday - Partly Cloudy - 9°C / 12°C",
+                "Saturday - Thunderstorms - 6°C / 7°C",
+                "Sunday - Rainy - 6°C / 8°C",
+                "Monday - Partly Cloudy - 3°C / 6°C",
+                "Tue, Nov 17 - Birthday - 1°C / 8°C",
+                "Wed, Nov 18 - Hangover - 2°C / 9°C",
+                "Thu, Nov 19 - Sunny - 0°C / 8°C",
+                "Fri, Nov 20 - Partly Cloudy - 0°C / 4°C",
+                "Sat, Nov 21 - Rainy - 6°C / 7°C",
+                "Sun, Nov 22 - Cloudy - 16°C / 8°C",
+                "Mon, Nov 23 - Rainy - 15°C / 10°C",
+        };
+
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for(String weather : testWeatherData){
+            mWeatherTextView.append(weather + "\n\n\n");
+        }
     }
 }
